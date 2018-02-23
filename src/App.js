a36371658f782726fe062bcb73b28e6f0db91847
Brandon Grissom <lousaydus@gmail.com>
2018-02-23 00:16:14 -0800

    First Commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, {Component} from 'react'
+import './styles.css'
+import { Container, Button } from 'semantic-ui-react'
+import { BrowserRouter as Router, Route, Link} from 'react-router-dom'
+
+const HeaderContainer = () => (
+  {empty: true}
+)
+
+export default class App extends Component {
+  render() {
+    return (
+      <Router>
+        <Container>
+          HEADER LOL YEAHHEADER LOL YEAHHEADER LOL YEAHHEADER LOL YEAHHEADER LOL YEAHHEADER LOL YEAHHEADER LOL YEAHHEADER LOL YEAHHEADER LOL YEAHHEADER LOL YEAHHEADER LOL YEAHHEADER LOL YEAHHEADER LOL YEAHHEADER LOL YEAHHEADER LOL YEAHHEADER LOL YEAHHEADER LOL YEAHHEADER LOL YEAHHEADER LOL YEAHHEADER LOL YEAHHEADER LOL YEAHHEADER LOL YEAHHEADER LOL YEAHHEADER LOL YEAHHEADER LOL YEAHHEADER LOL YEAHHEADER LOL YEAHHEADER LOL YEAH
+        </Container>
+      </Router>
+        )
+  }
+}
